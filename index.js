0b06f5b4ce23f66505d6a85ec699b8044b2fd4a3
Milo Koleske <144748100+miloKoleske@users.noreply.github.com>
2024-01-02 21:27:30 -0700

    Update index.js

--- index.js
+++ index.js
@@ -26,129 +26,147 @@ const fakeStore = async(endpoint) => {
     }
     displayCards(data);
     } 
-    
+
+
 const displayCards = data => {
     console.log(data);
-    data.forEach(element => {
+    data.forEach(item => {
         
 // * Create Elements of card
 let card = document.createElement('div');
 let categoryButton = document.createElement('h3');
-let description = document.createElement('p');
+
 let id = document.createElement('h5');
 let image = document.createElement('img');
 let body = document.createElement('div');
-let price = document.createElement('p');
+
 let title = document.createElement('h1');
 let btn = document.createElement('button');
+
+
+
 
 //  * Set attributes -- describing all parts in html
 card.className = 'card';
 card.style.width = '18rem';
-image.src = element.image;
+image.src = item.image;
 image.className = 'card-img-top';
 title.className = 'card-title';
-title.textContent = element.title;
-description.className = 'card-text';
-description.textContent = element.description;
-price.className = 'card-price';
-price.textContent = element.price;
+title.textContent = item.title;
 btn.className = 'btn btn-primary';
 btn.textContent = 'Add to Cart'
 btn.className = 'btn btn-outline-success me-2';
 btn.onclick = () => {
-    let element = {
-        id: element.id,
-        title: element.title,
-        cost: element.cost,
+    var product = {
+        id: item.id,
+        title: item.title,
+        price: item.price,
         quantity: 1
     }
-    cart.push(element);
-    submitToCart();
+    console.log(product);
+    cart.push(product);
+    displayCart(); // call the function by using var name
 }
-//console.log(cart);
 
 
 // * Append Elements -- appending all elements to each other -- go from bottom to top - add bottom to second to bottom, etc.
+
+
+// TODO: Create Accordion
+
+// Create elements
+let accordion = document.getElementById('accordionFlushExample');
+let desc = document.createElement('p');
+let price = document.createElement('p');
+desc.textContent = item.desc;
+price.textContent = item.price;
+
+
+// set attributes
+desc.className = item.desc;
+desc.textContent = item.desc;
+price.className = 'card-price';
+price.textContent = item.price;
+
+// append
 card.appendChild(image);
 card.appendChild(title);
-card.appendChild(description);
-card.appendChild(price);
 card.appendChild(btn);
+card.appendChild(desc);
+card.appendChild(price);
+
 display.appendChild(card);
 });
 }
-// TODO: Create Accordion
-
 
 
 // TODO: Display Cart
 const displayCart = () => {
     // use parameter to add to cart array
 console.log('Items in Cart: ', cart);
-cart.map(element => {
+cart.forEach(item => {
 
-    // * Create Elements of card
-let card = document.createElement('div');
-let image = document.createElement('image');
+    // * Create Elements of modal
 let price = document.createElement('p');
 let title = document.createElement('h1');
 let body = document.createElement('div');
+// document.getElementById('subtotalValue').textContent = "subtotal value goes here";
+let tableRow = document.createElement('tr');
+let itemName = document.createElement('td');
+let itemQuant = document.createElement('td');
+let itemPrice = document.createElement('td');
+let tbody = document.getElementById('tbody');
+itemPrice.textContent = item.price;
+itemName.textContent = item.title;
+itemQuant.textContent = 1;
+
+//! let desc = document.createElement('div')
 
 //  * Set attributes -- describing all parts in html
-card.className = 'card';
-card.style.width = '18rem';
-image.src = element.image;
-image.className = 'card-img-top';
-title.className = 'card-title';
-title.textContent = element.title;
-cardPrice.className = 'card-price';
-cardPrice.textContent = element.price;
 
-// * Append Elements -- appending all elements to each other -- go from bottom to top - add bottom to second to bottom, etc.
-p.appendChild(p);
-body.appendChild(p);
-body.appendChild(h1);
-card.appendChild(body);
-card.appendChild(image);
-d.appendChild(card);
-keptCards.appendChild(div);
+
+// * Append Elements -- appending all elements to each other
+tableRow.appendChild(itemQuant);
+tableRow.appendChild(itemName);
+tableRow.appendChild(itemPrice);
+tbody.appendChild(tableRow);
 })}
 
-// TODO: Create Accordion
 
 
-// TODO: Display Cart
-const submitToCart = () => {
+// TODO: addToCart
+// ! add an event listener that calls function to display cart
+const addToCart = () => {
 console.log('Items in Cart: ', cart);
-cart.map(element => {
+cart.map(item => {
 
     // * Create Elements of card
+let div = document.createElement('div');
 let card = document.createElement('div');
-let image = document.createElement('image');
+let image = document.createElement('img');
 let price = document.createElement('p');
 let title = document.createElement('h1');
-let body = document.createElement('div');
+
 
 //  * Set attributes -- describing all parts in html
 card.className = 'card';
 card.style.width = '18rem';
-image.src = element.image;
+image.src = item.image;
 image.className = 'card-img-top';
 title.className = 'card-title';
-title.textContent = element.title;
-cardPrice.className = 'card-price';
-cardPrice.textContent = element.price;
+title.textContent = item.title;
+price.className = 'card-price';
+price.textContent = item.price;
 
 // * Append Elements -- appending all elements to each other -- go from bottom to top - add bottom to second to bottom, etc.
-p.appendChild(p);
-body.appendChild(p);
-body.appendChild(h1);
-card.appendChild(body);
+card.appendChild(div);
 card.appendChild(image);
-div.appendChild(card);
-keptCards.appendChild(div);
-})
+card.appendChild(price); // left = parent -- right = child
+card.appendChild(title);
+
+});
+
+// for each item, add all values to the modal, and add up price of all cart items combined
 }
 
 // ! Event Listeners
